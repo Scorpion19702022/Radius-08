4878d420b48e3d869deffdd5f48a2820a38a9d8d
skorpion <jmosinski@o2.pl>
2023-03-29 21:26:08 +0200

    add code for nav in js

---
+++ js/script.js
@@ -0,0 +1,21 @@
+const burgerBtn = document.querySelector('.burger')
+const btnBars = document.querySelector('.fa-bars')
+const btnCross = document.querySelector('.fa-xmark')
+const nav = document.querySelector('.navbar')
+const links = document.querySelectorAll('.navbar__link')
+
+const handleNav = () => {
+	nav.classList.toggle('actionnav')
+	btnBars.classList.toggle('hide')
+	btnCross.classList.toggle('hide')
+
+	links.forEach(items => {
+		items.addEventListener('click', () => {
+			nav.classList.remove('actionnav')
+			btnBars.classList.remove('hide')
+			btnCross.classList.add('hide')
+		})
+	})
+}
+
+burgerBtn.addEventListener('click', handleNav)
