29d93e88b80dee88e949b5d1ee7e2f57c98a50c2
skorpion <jmosinski@o2.pl>
2023-04-07 21:51:13 +0200

    add styles and js for brakpoint medium

--- js/script.js
+++ js/script.js
@@ -12,6 +12,11 @@ const body = document.querySelector('body')
 const imgCardOne = document.querySelector('.imgcardone')
 const imgCardTwo = document.querySelector('.imgcardtwo')
 
+const cardAboutOne = document.querySelector('.aboutone')
+const cardAboutTwo = document.querySelector('.abouttwo')
+const cardAboutThree = document.querySelector('.aboutthree')
+const cardAboutFour = document.querySelector('.aboutfour')
+
 body.onload = function () {
 	loadCards()
 }
@@ -29,6 +34,19 @@ const loadCards = () => {
 	setTimeout(() => {
 		imgCardTwo.classList.add('imgcardoneview')
 	}, 3200)
+
+	setTimeout(() => {
+		cardAboutOne.classList.add('actioncardsaboutus')
+	}, 500)
+	setTimeout(() => {
+		cardAboutTwo.classList.add('actioncardsaboutus')
+	}, 1500)
+	setTimeout(() => {
+		cardAboutThree.classList.add('actioncardsaboutus')
+	}, 1000)
+	setTimeout(() => {
+		cardAboutFour.classList.add('actioncardsaboutus')
+	}, 2000)
 }
 
 // ------------------------------------------
