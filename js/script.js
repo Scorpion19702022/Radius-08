9d5204c050875c049c549b414d5d090662fed3e3
skorpion <jmosinski@o2.pl>
2023-04-22 21:20:20 +0200

    corrected code in js

--- js/script.js
+++ js/script.js
@@ -103,7 +103,8 @@ const scrollOffer = () => {
 if (document.location.search === '?mail_status=sent') {
 	msgStatus.classList.add('sendsuccess')
 	msgStatus.textContent = 'Wiadomość wysłana poprawnie'
-} else {
+}
+if (document.location.search === '?mail_status=error') {
 	msgStatus.classList.add('senderror')
 	msgStatus.textContent = 'Nie udało się wysłać wiadomości'
 }
