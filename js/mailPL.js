a1e21d02ccca0e91aa28a7874133f5eee04238fc
skorpion <jmosinski@o2.pl>
2023-04-23 20:44:18 +0200

    form validation in js

--- js/mailPL.js
+++ js/mailPL.js
@@ -11,27 +11,65 @@ const errorText = document.querySelector('.errortext')
 const btnSend = document.querySelector('.btnsend')
 const btnClear = document.querySelector('.btnclean')
 
-console.log(document.location.search)
+// console.log(document.location.search)
 
-if (document.location.search === '?mail_status=sent') {
-	msgStatus.classList.add('sendsuccess')
-	msgStatus.textContent = 'Wiadomość wysłana poprawnie'
-	setTimeout(() => {
-		msgStatus.classList.remove('sendsuccess')
-	}, 3000)
-}
-if (document.location.search === '?mail_status=error') {
-	msgStatus.classList.add('senderror')
-	msgStatus.textContent = 'Nie udało się wysłać wiadomości'
-	setTimeout(() => {
-		msgStatus.classList.remove('senderror')
-	}, 3000)
+// if (document.location.search === '?mail_status=sent') {
+// 	msgStatus.classList.add('sendsuccess')
+// 	msgStatus.textContent = 'Wiadomość wysłana poprawnie'
+// 	setTimeout(() => {
+// 		msgStatus.classList.remove('sendsuccess')
+// 	}, 3000)
+// }
+// if (document.location.search === '?mail_status=error') {
+// 	msgStatus.classList.add('senderror')
+// 	msgStatus.textContent = 'Nie udało się wysłać wiadomości'
+// 	setTimeout(() => {
+// 		msgStatus.classList.remove('senderror')
+// 	}, 3000)
+// }
+
+const send = () => {
+	if (inputName.value !== '' && inputMail.value !== '' && textarea.value !== '') {
+		msgStatus.classList.add('sendsuccess')
+		msgStatus.textContent = 'Wiadomość wysłana poprawnie'
+		setTimeout(() => {
+			msgStatus.classList.remove('sendsuccess')
+		}, 3000)
+	} else if (inputName.value === '') {
+		errorName.classList.add('actionerror')
+		errorName.textContent = 'Wypełnij pole'
+		msgStatus.classList.add('senderror')
+		msgStatus.textContent = 'Nie udało się wysłać wiadomości'
+		setTimeout(() => {
+			msgStatus.classList.remove('senderror')
+		}, 3000)
+	} else if (inputMail.value === '') {
+		errorMail.classList.add('actionerror')
+		errorMail.textContent = 'Wypełnij pole'
+		msgStatus.classList.add('senderror')
+		msgStatus.textContent = 'Nie udało się wysłać wiadomości'
+		setTimeout(() => {
+			msgStatus.classList.remove('senderror')
+		}, 3000)
+	} else if (textarea.value === '') {
+		errorText.classList.add('actionerror')
+		errorText.textContent = 'Wypełnij pole'
+		msgStatus.classList.add('senderror')
+		msgStatus.textContent = 'Nie udało się wysłać wiadomości'
+		setTimeout(() => {
+			msgStatus.classList.remove('senderror')
+		}, 3000)
+	}
 }
 
 const clean = () => {
 	inputName.value = ''
 	inputMail.value = ''
 	textarea.value = ''
+	errorName.classList.remove('actionerror')
+	errorMail.classList.remove('actionerror')
+	errorText.classList.remove('actionerror')
 }
 
+btnSend.addEventListener('click', send)
 btnClear.addEventListener('click', clean)
